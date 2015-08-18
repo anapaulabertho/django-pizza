3d9234e2a14c7755dc839e94d4d87060e4e01e0d
Ana Paula Bertho <paula_bertho@hotmail.com>
2015-08-18 10:47:27 -0300

    loops em py

---
+++ pyexamples/loops.py
@@ -0,0 +1,13 @@
+pedidos = [
+    {
+        'nome': 'Mario',
+        'sabor': 'Portuguesa'
+    },
+    {
+        'nome': 'Marco',
+        'sabor': 'Presunto'
+    }
+]
+
+for pedido in pedidos:
+    print(pedido['nome'], pedido['sabor'])
